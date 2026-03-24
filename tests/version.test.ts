a1003fbac957aabce7775711c67058a1eb6d3607
Wepo <alexandre.mourot@epfl.ch>
2026-03-24 10:16:36 +0100

    test: add package.json field validation (181 tests)

--- tests/version.test.ts
+++ tests/version.test.ts
@@ -6,4 +6,18 @@ describe("Package version", () => {
 		expect(pkg.version).toMatch(/^\d+\.\d+\.\d+$/);
 		expect(pkg.name).toBe("0g-agent-kit");
 	});
+
+	test("has required package fields", () => {
+		const pkg = require("../package.json");
+		expect(pkg).toHaveProperty("description");
+		expect(pkg).toHaveProperty("license", "MIT");
+		expect(pkg).toHaveProperty("dependencies");
+		expect(pkg.description.length).toBeGreaterThan(10);
+	});
+
+	test("has ethers and zod as dependencies", () => {
+		const pkg = require("../package.json");
+		expect(pkg.dependencies).toHaveProperty("ethers");
+		expect(pkg.dependencies).toHaveProperty("zod");
+	});
 });
